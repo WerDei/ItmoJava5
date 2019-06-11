cca25a6eb2154484f50628861c01d99b51c84b5a
WerDei <werdei.mail@gmail.com>
2019-06-11 16:20:53 +0300

    Хеширование

--- modules/server/src/auth/PasswordHasher.java
+++ modules/server/src/auth/PasswordHasher.java
@@ -1,9 +1,26 @@
 package net.werdei.talechars.server.auth;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Base64;
+
 public class PasswordHasher
 {
+    static final int hashLength = 512;
+
     public static String hash(String password)
     {
-        return password;
+        String hash;
+        ByteBuffer buf = ByteBuffer.allocate(hashLength/8);
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(md.digest());
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 }
